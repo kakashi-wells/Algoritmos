a96699a56559c58a8024e4c426905a9df94929ac
kakashi-hatake <arthurbedoya99@gmail.com>
2021-07-10 01:40:26 -0500

    Ejemplo de pseudocodigo

---
+++ pseudocodigo.py
@@ -0,0 +1,8 @@
+print ("Ingresa el primer precio")
+n1 = int(input())
+print ("Ingresa el segundo precio")
+n2 = int(input())
+print ("Ingresa el tercer precio")
+n3 = int(input())
+preciopromedio = (n1 + n2 + n3)/3
+print ("El precio medio de su producto es" + "" + str(preciopromedio))
